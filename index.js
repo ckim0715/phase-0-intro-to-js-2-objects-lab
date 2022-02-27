4fb1ce8bf7dc4f3021f1b61c9cd3cc6d0dfa965b
Christopher <christopher.kim43@gmail.com>
2022-02-27 16:02:36 -0500

    commit tree 57cc4072313364a8d15291a62b72315171ba74a4

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+
+
 // Write your solution in this file!
+const employee = {
+    name: "Christopher Kim",
+    streetAddress: "555 Gorge Road", 
+    
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, 
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+
+
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployees = {...employee};
+    delete newEmployees[key];
+    return newEmployees;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
